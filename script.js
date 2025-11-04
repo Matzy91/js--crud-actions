ed196a52b828e1de6269a8f9347bc9f273b72b90
Mattias <128178112+Matzy91@users.noreply.github.com>
2025-11-04 11:53:46 +0100

    Co-authored-by: bruschristoffer <bruschristoffer@users.noreply.github.com> Co-authored-by: jprecenth <jprecenth@users.noreply.github.com> Co-authored-by: Wenjie <eijnewe@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-const BASE_URL = "https://ca47d565dabb0dd6c0a5.free.beeceptor.com/api";
+    const BASE_URL = "http://localhost:3000";
+
+    
+// Create 
+async function createUser(username, email, password) {
+    
+    return fetch(`${BASE_URL}/users`, {
+        method: 'POST',
+        body: JSON.stringify({username, email, password})
+    }).then((res) => res.json())
+}
+
+//createUser("shialabeouf", "shia@mybutt.ass", "iluvmeganfox").then((data) =>   console.log(data))
+
+// Read
+async function fetchUsers() {
+    return fetch(`${BASE_URL}/users`).then((res) => res.json());
+    }
+const data = await fetchUsers();
+console.log(data);
+
+
+// Update
+async function updateUser({ id, username, email, password }){
+    return fetch(`${BASE_URL}/users/${id}`, {
+        method: 'PATCH',
+        body: JSON.stringify({ username, email, password })
+    }).then((res) => res.json());
+}
+
+
+updateUser({id: "9d1e", password: "poop234"}).then((data) => console.log(data));
+
+
+// Delete
+async function deleteUser(id) {
+    return fetch(`${BASE_URL}/users/${id}`, {
+        method: "DELETE",
+    }).then((res) => res.json());
+}
+deleteUser("3a8e").then((data) => console.log(data));
+// deleteUser("7ad5e6fc3149742f1292").then((data) => console.log(data));
